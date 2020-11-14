b9ec24b231d40e85eef3690f39f09a8cd956bdfc
starbuckshiker <u1304954@umail.utah.edu>
2020-11-14 15:57:43 -0700

    ?

--- loops.js
+++ loops.js
@@ -17,7 +17,7 @@ for (;fruit[i];) {
     i++;
     console.log(fruit[i]);
 }
-
+     // Bronze
  var numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
  for(var i = 0; i < numbers.length; i++){
         console.log(String(numbers))
@@ -28,9 +28,32 @@ for (;fruit[i];) {
    console.log(i)
  }
 
-
-    var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+   
+ var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
          for(var i = 0; i < fruit.length; i = i + 2){
               console.log(Array(fruit[i]))
          }
  
+         // Silver
+ var people = [
+    {
+    name: "Harrison Ford",
+    occupation: "Actor"
+    },
+    {
+    name: "Justin Bieber",
+    occupation: "Singer"
+    },
+    {
+    name: "Vladimir Putin",
+    occupation: "Politician"
+    },
+    {
+    name: "Oprah",
+    occupation: "Entertainer"
+    }
+  ] 
+   
+  for(var i = 0; i < people.length; i++){
+    console.dir(people.name.)
+  }
