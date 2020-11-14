47c7b930b3af9c0259fc253d16f869b203ed0167
starbuckshiker <u1304954@umail.utah.edu>
2020-11-14 00:13:36 -0700

    boo!

---
+++ loops.js
@@ -0,0 +1,34 @@
+var s = "";
+for(var i = 0; i < 9; i += 1) {
+  s += i + " ";
+  console.log(i)
+}
+var s = "";
+for(var i = 10; i > 0; i --) {
+  s += i + " ";
+  console.log(i)
+}
+
+var fruit = ["1", "banana", "orange", "apple", "kiwi"]
+var i = 0; 
+var text = ""; 
+for (;fruit[i];) {
+    text += fruit[i]; i < 3; 
+    i++;
+    console.log(fruit[i]);
+}
+
+ var numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ for(var i = 0; i < numbers.length; i++){
+        console.log(String(numbers))
+ }
+ var s = "";
+ for(var i = 0; i < 101; i += 2) {
+   s += i + " ";
+   console.log(i)
+ }
+
+ var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+ for(var i = 0; i < fruit.length; i ++){
+        console.log(String(fruit));
+ }
